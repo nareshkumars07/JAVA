2df76912299bc40ee2f74e26d0c7dda4c79d4798
nareshkumars07 <nareshksundar@gmail.com>
2025-06-26 16:17:12 +0530

    Create Do While Even Number.java

---
+++ JAVA BASICS/Do While Even Number.java
@@ -0,0 +1,10 @@
+// DO WHILE EVEN NUMBER
+class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println(i);
+            i+=2;
+        }while(i<=10);
+    }
+}
